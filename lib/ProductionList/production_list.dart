b59602c46a5c04dc9a0a19eb1bc1816f1b87c310
Temofeyy <temofeyy.20@gmail.com>
2023-06-29 20:55:53 +0300

    refactored production_list.dart

--- lib/ProductionList/production_list.dart
+++ lib/ProductionList/production_list.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
 import 'package:provider/provider.dart';
 
+import '../widgets/tile.dart';
+
 class ProductionList extends StatefulWidget {
   final String itemSource;
   final GlobalKey<SliderDrawerState> _sliderDrawerKey =
@@ -27,38 +29,36 @@ class _ProductionListState extends State<ProductionList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        leading: IconButton(
-          icon: Icon(Icons.menu, color: Colors.white),
-          onPressed: () {
-            widget._sliderDrawerKey.currentState?.toggle();
-          },
-        ),
+        leading: _AppBarMenuButton(menuKey:widget._sliderDrawerKey),
         title: Text(
           'Production List',
-          style: TextStyle(color: Colors.white),
+          style: Theme.of(context).textTheme.titleMedium,
         ),
       ),
       body: SafeArea(
         child: Consumer<OrderProvider>(builder: (_, provider, __) {
           filteredItems = provider.getFilteredOrderedItems(widget.itemSource);
           return SliderDrawer(
-            appBar: const SizedBox.shrink(),
+            appBar: null,
             key: widget._sliderDrawerKey,
             sliderOpenSize: 250,
             slider: SliderView(onItemClick: (title) {
               // Handle the menu item click as necessary
             }),
-            child: provider.isLoading
-                ? _loadingIndicator()
-                : filteredItems.isNotEmpty
-                    ? _productionList()
-                    : _emptyListPlaceHolder(),
+            child: ColoredBox(
+              color: Theme.of(context).scaffoldBackgroundColor,
+              child: provider.isLoading
+                  ? _loadingIndicator()
+                  : filteredItems.isNotEmpty
+                      ? _productionList()
+                      : _emptyListPlaceHolder(),
+            ),
           );
         }),
       ),
     );
   }
+
 
   Widget _loadingIndicator() {
     return Center(
@@ -74,16 +74,7 @@ class _ProductionListState extends State<ProductionList> {
       itemCount: filteredItems.length,
       itemBuilder: (context, index) {
         OrderedItem item = filteredItems[index];
-        return ListTile(
-          title: Text(
-            item.productName,
-            style: TextStyle(color: Colors.black),
-          ),
-          subtitle: Text(
-            'Quantity: ${item.quantity}',
-            style: TextStyle(color: Colors.black),
-          ),
-        );
+        return _ProductionListItem(item: item);
       },
     );
   }
@@ -100,3 +91,52 @@ class _ProductionListState extends State<ProductionList> {
     );
   }
 }
+class _AppBarMenuButton extends StatelessWidget {
+  final GlobalKey<SliderDrawerState> menuKey;
+
+  const _AppBarMenuButton({Key? key, required this.menuKey}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.menu, color: Colors.white),
+      onPressed: () {
+        if(!menuKey.currentState!.isDrawerOpen){
+          menuKey.currentState?.toggle();
+
+        }
+      },
+    );
+  }
+}
+
+class _ProductionListItem extends StatelessWidget {
+  final OrderedItem item;
+  const _ProductionListItem({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Tile(
+      height: 100,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            item.productName,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          Text(
+            'Product ID: ${item.productId}',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          Text(
+            'Quantity: ${item.quantity}',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
