940e50b743750ef382bf2bf60ada1bbadf5ff90a
Temofeyy <temofeyy.20@gmail.com>
2023-06-30 20:26:46 +0300

    added outline border to UI kit

--- lib/assets/ui.dart
+++ lib/assets/ui.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class UIConstants {
+  UIConstants._();
   static const BACKGROUND_COLOR = Color(0xff181A20);
   static const TEXT_COLOR = Color(0xff707487);
   static const TEXT_FIELD_COLOR = Color(0xff262A34);
@@ -13,11 +14,15 @@ class UIConstants {
   static const GREY = Color(0xff383B45);
   static const GREY_MEDIUM = Color(0xff1F222A);
   static const GREY_LIGHT = Color(0xff262A34);
+  static const GREY_BORDER = Color(0xff333642);
 
   static const BLUE = Color(0xff246BFE);
   static const GREEN = Color(0xff3DA74E);
   static const ORANGE = Color(0xffEFAF4B);
   static const RED = Colors.red;
 
-
+  static InputBorder FIELD_BORDER = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(12),
+    borderSide: const BorderSide(color: UIConstants.GREY_BORDER),
+  );
 }
