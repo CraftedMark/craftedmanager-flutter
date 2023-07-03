7c4e0160346fe1fc9393756ca2c6a67944922b02
Temofeyy <temofeyy.20@gmail.com>
2023-07-03 20:27:03 +0300

    finished contact_lists.dart new UI

--- lib/Contacts/contact_lists.dart
+++ lib/Contacts/contact_lists.dart
@@ -1,9 +1,12 @@
 import 'package:crafted_manager/Contacts/people_db_manager.dart';
+import 'package:crafted_manager/widgets/divider.dart';
+import 'package:crafted_manager/widgets/plus_button.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
 import '../Models/people_model.dart';
 import '../WooCommerce/woosignal-service.dart';
+import '../assets/ui.dart';
 import '../config.dart';
 import '../widgets/edit_button.dart';
 import '../widgets/text_input_field.dart';
@@ -65,54 +68,71 @@ class _ContactsListState extends State<ContactsList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Row(
-          children: [
-            Expanded(
-              child: TextInputField(
-                initialValue: '',
-                labelText: 'Search Contacts...',
-                onChange: (value) {
-                  setState(() {
-                    _filteredContacts = _contacts
-                        .where((contact) =>
-                            contact.firstName
-                                .toLowerCase()
-                                .contains(value.toLowerCase()) ||
-                            contact.lastName
-                                .toLowerCase()
-                                .contains(value.toLowerCase()))
-                        .toList();
-                  });
+        backgroundColor: UIConstants.GREY_MEDIUM,
+        title: const Text('Search People'),
+        bottom: PreferredSize(
+          preferredSize: const Size(double.infinity, 64),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+            child: SizedBox(
+              height: 46,
+              child: TextField(
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: UIConstants.WHITE_LIGHT),
+                onChanged: (query) {
+                  _filteredContacts = _contacts
+                      .where((contact) =>
+                          contact.firstName
+                              .toLowerCase()
+                              .contains(query.toLowerCase()) ||
+                          contact.lastName
+                              .toLowerCase()
+                              .contains(query.toLowerCase()))
+                      .toList();
+                  setState(() {});
                 },
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: UIConstants.SEARCH_BAR_COLOR,
+                  labelText: 'Search',
+                  labelStyle: Theme.of(context).textTheme.bodyMedium,
+                  enabledBorder: UIConstants.FIELD_BORDER,
+                  focusedBorder: UIConstants.FIELD_BORDER,
+                ),
               ),
             ),
-          ],
+          ),
         ),
         actions: [
-          EditButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ContactDetailWidget(
-                    contact: People.empty(),
-                    // Create a new contact with default values
-                    refresh: refreshContacts,
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: PlusButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ContactDetailWidget(
+                      contact: People.empty(),
+                      // Create a new contact with default values
+                      refresh: refreshContacts,
+                    ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
         ],
-        backgroundColor: Colors.black,
       ),
       body: _filteredContacts.isEmpty
           ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
+          : ListView.separated(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 32),
               itemCount: _filteredContacts.length,
               itemBuilder: (BuildContext context, int index) {
+                final textStyle = Theme.of(context).textTheme.bodySmall;
                 final contact = _filteredContacts[index];
                 return Dismissible(
                   key: Key(contact.id.toString()),
@@ -128,38 +148,77 @@ class _ContactsListState extends State<ContactsList> {
                     ),
                   ),
                   onDismissed: (direction) {
-                    deleteCustomer(contact);
+                    // deleteCustomer(contact);
                   },
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ContactDetailWidget(
-                            contact: contact,
-                            refresh: refreshContacts,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: GestureDetector(
+                      behavior: HitTestBehavior.opaque,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ContactDetailWidget(
+                              contact: contact,
+                              refresh: refreshContacts,
+                            ),
                           ),
-                        ),
-                      );
-                    },
-                    child: ListTile(
-                      title: _filteredContacts != null
-                          ? Text('${contact.firstName} ${contact.lastName}')
-                          : CircularProgressIndicator(),
-                      subtitle: Tile(
+                        );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text('Phone: ${parsePhoneNumber(contact.phone)}'),
-                            Text('Email: ${contact.email}'),
-                            Text('Brand: ${contact.brand}'),
+                            Text(
+                              '${contact.firstName} ${contact.lastName}',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium
+                                  ?.copyWith(color: UIConstants.WHITE_LIGHT),
+                            ),
+                            const SizedBox(height: 6),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Phone:', style: textStyle),
+                                Text(parsePhoneNumber(contact.phone),
+                                    style: textStyle),
+                              ],
+                            ),
+                            const SizedBox(height: 4),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Email:', style: textStyle),
+                                Text(contact.email, style: textStyle),
+                              ],
+                            ),
+                            const SizedBox(height: 4),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Brand:', style: textStyle),
+                                Text(contact.brand ?? '-', style: textStyle),
+                              ],
+                            ),
                           ],
                         ),
                       ),
                     ),
                   ),
                 );
-              }),
+              },
+              separatorBuilder: (_, __) {
+                return const Column(
+                  children: [
+                    SizedBox(height: 4),
+                    DividerCustom(),
+                    SizedBox(height: 4),
+                  ],
+                );
+              },
+            ),
     );
   }
 }
